0b243c596b995a6eee687e2a0547de0cf3cf9813
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 19:30:07 -0400

    Agregar archivos de proyecto.

---
+++ numeros positivos o negativos/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinar si un numero es negativo y positivo 
+
+            Console.WriteLine("Ingrese un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero == 0) 
+            {
+                Console.WriteLine("el numero " + numero +" es neutro");
+            }
+            else if(numero > 0)
+            {
+                Console.WriteLine("el numero " + numero + " es positivo ");
+            }
+            else {
+                Console.WriteLine("el numero " + numero + " es negativo ");
+            }
+            Console.ReadLine();
+        }
+    }
